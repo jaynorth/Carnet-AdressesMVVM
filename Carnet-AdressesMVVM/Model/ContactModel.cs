17aa196015eb753a2bfd5496ab2ee94580297f1c
jay <jayrdee@gmail.com>
2017-05-01 11:50:44 +0200

    created ContactModel class

---
+++ Carnet-AdressesMVVM/Model/ContactModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnet_AdressesMVVM.Model
+{
+    internal class ContactModel
+    {
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string Adresse { get; set; }
+        public string Fonction { get; set; }
+        public string Societe { get; set; }
+    }
+}
